343fb780ebbfadf5de501578e7396dad2da344aa
Rohit Gurnani <rkgi10@gmail.com>
2014-12-20 16:11:37 +0530

    Added Codemirror

---
+++ codeshareindex.js
@@ -0,0 +1,7 @@
+var myCodeMirror = CodeMirror(document.body, {
+  value: "function myScript(){return 100;}\n",
+  mode:  "javascript"
+});
+/*var myTextArea=document.querySelector("#mastertext");
+var myCodeMirror = CodeMirror.fromTextArea(myTextArea);
+*/
